7fe8cde26b1f37eba98427ee0b4e533b70487f68
Vladislav Shishkin <vladislavsiskin95@gmail.com>
2023-11-28 20:19:43 +0300

    начал header

---
+++ src/img/icons/arrow.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const arrow = () => {
+  return (
+    <svg
+      width="25"
+      height="25"
+      viewBox="0 0 25 25"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M8.94076 20.5157L15.4608 13.9957C16.2308 13.2257 16.2308 11.9657 15.4608 11.1957L8.94076 4.67566"
+        stroke="#E6F3F1"
+        strokeWidth="1.5"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default arrow;
---
+++ src/ui/buttons/Button.tsx
@@ -0,0 +1,61 @@
+import React, { ReactElement, ReactNode } from "react";
+import classNames from "classnames/bind";
+import styles from "./style.module.css";
+import clsx from "clsx";
+import Arrow from "@/img/icons/arrow";
+import { Nunito_Sans } from "next/font/google";
+
+const nunitoSans = Nunito_Sans({
+  subsets: ["latin"],
+});
+type ButtonType = "transparent" | "default";
+
+interface ButtonProps {
+  type: ButtonType;
+  children: ReactNode;
+  buttonType?: "button" | "submit";
+  style?: string;
+  disabled?: boolean;
+  onClick?: (e: any) => void;
+  isArrow?: boolean;
+}
+
+export const Button: React.FC<ButtonProps> = ({
+  type,
+  children,
+  onClick,
+  style,
+  buttonType = "button",
+  isArrow = false,
+}: ButtonProps): ReactElement => {
+  const cx = classNames.bind(styles);
+  const className = cx(
+    "button",
+    { [`button-${type}`]: type },
+    style,
+    nunitoSans.className
+  );
+
+  const clickHandler = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ): void => {
+    if (onClick) {
+      event.preventDefault();
+      event.stopPropagation();
+      onClick(event);
+    }
+  };
+
+  return (
+    <div className={styles.btnWrapper}>
+      {isArrow ? <Arrow /> : null}
+      <button
+        type={buttonType}
+        className={clsx(className)}
+        // onClick={clickHandler}
+      >
+        {children}
+      </button>
+    </div>
+  );
+};
